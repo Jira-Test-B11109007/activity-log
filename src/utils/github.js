e61429809c1e3ce70fd6dcc1e3728555a598d1ee
timo9378 <132542914+timo9378@users.noreply.github.com>
2024-10-18 14:34:19 +0800

    Update github.js

--- src/utils/github.js
+++ src/utils/github.js
@@ -113,13 +113,13 @@ async function fetchAndFilterEvents() {
                 return event;
             })
             .slice(0, eventLimit);
-//預更改位置
-        if (filteredEvents.length < eventLimit) {
-            const additionalEvents = await fetchAllEvents();
-            allEvents = additionalEvents.concat(allEvents);
-        } else {
-            break;
-        }
+        break;
+//        if (filteredEvents.length < eventLimit) {
+//            const additionalEvents = await fetchAllEvents();
+//            allEvents = additionalEvents.concat(allEvents);
+//        } else {
+//            break;
+//        }
     }
 
     filteredEvents = filteredEvents.slice(0, eventLimit);
