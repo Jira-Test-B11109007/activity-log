bcaddebb92d306847915add2e09ee42d71ff82a5
timo9378 <132542914+timo9378@users.noreply.github.com>
2024-10-18 12:00:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+const { fetchAndFilterEvents } = require('./utils/github');
+const { updateReadme } = require('./utils/file');
+const { username, token, eventLimit, ignoreEvents, readmePath, commitMessage } = require('./config');
+const core = require('@actions/core')
+
+// Main function to execute the update process
+async function main() {
+    try {
+        const activity = await fetchAndFilterEvents({ username, token, eventLimit, ignoreEvents });
+        await updateReadme(activity, readmePath);
+    } catch (error) {
+        core.setFailed(`❌ Error in the update process: ${error.message}`);
+        console.error(error)
+        process.exit(1);
+    }
+}
+
+// Execute the main function
+// Test123
+main();
 
